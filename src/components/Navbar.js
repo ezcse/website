4156193b4f6c99c3db5a3952a5bebe5058c29183
Ray <codecoffeecult@gmail.com>
2025-01-03 19:22:31 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,25 +9,10 @@ import { singlePage } from '../info/Info';
 
 const links = [
     {
-        name: 'Home',
-        to: '',
-        active: 'home'
-    },
-    {
-        name: 'About Me',
-        to: 'about',
-        active: 'about'
-    },
-    {
         name: info.initials,
         type: 'initials',
         to: '',
         active: 'home'
-    },
-    {
-        name: 'Portfolio',
-        to: 'portfolio',
-        active: 'portfolio'
     }
 ]
 
